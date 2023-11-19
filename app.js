f284b10510ca9f0090854dceccd1d44e110c0f97
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-11-19 15:01:39 -0500

    add the render link to app.js for socket.io connection

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const app = express();
 
 // MIDDLEWARE
 app.use(cors({
-  origin: 'http://localhost:3000', // Frontend URL
+  // origin: 'http://localhost:3000', // Frontend URL
+  origin: 'https://swif.onrender.com', // Frontend URL
   credentials: true
 }));
 app.use(express.json());
